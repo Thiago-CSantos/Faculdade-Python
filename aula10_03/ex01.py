333180ad6ef28326ea958a6deb7edbd175661b8c
Thiago Carretero <thiagocarretero433@gmail.com>
2023-03-12 17:35:44 -0300

    Exercicio 09

--- aula10_03/ex01.py
+++ aula10_03/ex01.py
@@ -3,10 +3,6 @@ lista = [1, 2, 3, 6, 69, 44]
 print(lista)
 
 
-# 9. O que é impresso pelo trecho de código a seguir? [[], 3.14, False]
-lista = [3, 69, "gato", [56, 57, 'cachorro'], [], 3.14, False]
-print(lista[4:])
-
 # 10. O que é impresso pelo trecho de código a seguir? [False, 4, 2, True, 8, 6, 5]
 lista = [4, 2, 8, 6, 5]
 lista.insert(2, True)
---
+++ aula10_03/ex09.py
@@ -0,0 +1,3 @@
+# 9. O que é impresso pelo trecho de código a seguir? [[], 3.14, False]
+lista = [3, 69, "gato", [56, 57, 'cachorro'], [], 3.14, False]
+print(lista[4:])

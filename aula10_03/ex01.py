d7eef2674fd24a0aa522584ecffb428af6dec358
Thiago Carretero <thiagocarretero433@gmail.com>
2023-03-12 17:36:16 -0300

    Exercicio 10

--- aula10_03/ex01.py
+++ aula10_03/ex01.py
@@ -1,12 +1,3 @@
 # 1. Uma lista só pode conter números inteiros? VERDADEIRO
 lista = [1, 2, 3, 6, 69, 44]
 print(lista)
-
-
-# 10. O que é impresso pelo trecho de código a seguir? [False, 4, 2, True, 8, 6, 5]
-lista = [4, 2, 8, 6, 5]
-lista.insert(2, True)
-lista.insert(0, False)
-print(lista)
-
-
---
+++ aula10_03/ex10.py
@@ -0,0 +1,5 @@
+# 10. O que é impresso pelo trecho de código a seguir? [False, 4, 2, True, 8, 6, 5]
+lista = [4, 2, 8, 6, 5]
+lista.insert(2, True)
+lista.insert(0, False)
+print(lista)

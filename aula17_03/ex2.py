a2391f97697131a7ef0030cb8653bc4463cbf57f
Thiago Carretero <thiagocarretero433@gmail.com>
2023-03-18 01:29:05 -0300

    ex2

---
+++ aula17_03/ex2.py
@@ -0,0 +1,8 @@
+# ex2
+media = float(input("Digite a media do aluno"))
+if(media >= 7 ):
+    print("A media do aluno é ", media)
+    print("Aprovado")
+else:
+    print("A media do aluno é ", media)
+    print("Reprovado")

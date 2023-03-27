df38d3032b65210584c06a02b562d752a246014f
Thiago Carretero <thiagocarretero433@gmail.com>
2023-03-26 21:49:57 -0300

    ex06

---
+++ listas exercicios/listas, strings e dicionario/ex6.py
@@ -0,0 +1,4 @@
+s = 'If you have an apple and I have an apple and we exchange these apples then you and I will still each have one apple. But if you have an idea and I have an idea and we exchange these ideas, then each of us will have two ideas.'
+
+x = s.split(' ')
+print(len(x))
